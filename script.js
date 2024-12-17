260bc866b371cc6f4513277a2dabba5029249cd5
Elisei <eliseivlas.002@gmail.com>
2024-12-17 15:51:36 +0100

    calcolo del prezzo in base al eta del passegerro

--- script.js
+++ script.js
@@ -18,12 +18,28 @@
     let prezzoBiglietto = (killometriDaPercorrere*prezzoPerKillometro)
     console.log(prezzoBiglietto)
 
+        //calcolo da fare nel caso eta inferiore a 18 anni
+        let scontoInferiore = (prezzoBiglietto*80/100);
+        
+
+        //calcolo da fare nel caso eta superiore a 64 anni
+        let scontoSuperiore = (prezzoBiglietto*60/100)
+        
+    
     // calcolo lo sconto in base all'eta del passegero
         //SE l'eta del passeggero e inferiore a 18 anni ha uno sconto del 20%
-
-        //ALTRIMENTI SE l'eta del passeggero e superiore a 64 anni ha uno sconto del 40%
-
-        //Altrimenti il prezzo non viene scontato
+        if (etaDelPasseggero < 18) {
+            //sconto del 20%
+            prezzo = scontoInferiore
+            //ALTRIMENTI SE l'eta del passeggero e superiore a 64 anni ha uno sconto del 40%
+        }else if(etaDelPasseggero > 64) {
+            //sconto del 40%
+            prezzo = scontoSuperiore
+            //Altrimenti il prezzo rimane non scontato
+        }else {
+            prezzo = prezzoBiglietto
+        }
 
 //OUTPUT
     // prezzo finale 
+    console.log("Il prezzo da pagare e" + " " + prezzo + "€")
