a0ad6dc2ccabc9d78c62eb4512adbfddc1bc0ad5
Elisei <eliseivlas.002@gmail.com>
2024-12-17 15:26:13 +0100

    calcolo il prezzo del percorso

--- script.js
+++ script.js
@@ -3,15 +3,20 @@
 // INPUT
     // prezzo biglietto in base ai km
 
-    let prezzoBiglietto = 0.21 + "€";
-    console.log(prezzoBiglietto);
+    let prezzoPerKillometro = 0.21;
 
     // l'utente inserisce il numero di killometri che vuole percorrere
     let killometriDaPercorrere = prompt("Inserisca i killometri che deve percorrere");
+
     //l'utente inserisce la sua eta
-    let    
+    let etaDelPasseggero = prompt("Inserisca la sua eta") 
+
+
+    console.log(prezzoPerKillometro, killometriDaPercorrere, etaDelPasseggero);
 //ELABORAZIONE
     // calcolo il prezzo del percorso in base al killometraggio
+    let prezzoBiglietto = (killometriDaPercorrere*prezzoPerKillometro)
+    console.log(prezzoBiglietto)
 
     // calcolo lo sconto in base all'eta del passegero
         //SE l'eta del passeggero e inferiore a 18 anni ha uno sconto del 20%
