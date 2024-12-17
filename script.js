22feb08f66d2760854f4dbede8d476767eb0d676
Elisei <eliseivlas.002@gmail.com>
2024-12-17 15:17:40 +0100

    Logica del compito

---
+++ script.js
@@ -0,0 +1,24 @@
+// Biglietto del treno
+
+// INPUT
+    // prezzo biglietto in base ai km
+
+    let prezzoBiglietto = 0.21 + "€";
+    console.log(prezzoBiglietto);
+
+    // l'utente inserisce il numero di killometri che vuole percorrere
+    let killometriDaPercorrere = prompt("Inserisca i killometri che deve percorrere");
+    //l'utente inserisce la sua eta
+    let    
+//ELABORAZIONE
+    // calcolo il prezzo del percorso in base al killometraggio
+
+    // calcolo lo sconto in base all'eta del passegero
+        //SE l'eta del passeggero e inferiore a 18 anni ha uno sconto del 20%
+
+        //ALTRIMENTI SE l'eta del passeggero e superiore a 64 anni ha uno sconto del 40%
+
+        //Altrimenti il prezzo non viene scontato
+
+//OUTPUT
+    // prezzo finale 
