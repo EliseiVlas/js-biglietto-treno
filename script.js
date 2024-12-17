3cc9b2ae52759b45221c412f97f26121c54fa22b
Elisei <eliseivlas.002@gmail.com>
2024-12-17 16:18:58 +0100

    consegna esercizio

--- script.js
+++ script.js
@@ -16,7 +16,9 @@
 //ELABORAZIONE
     // calcolo il prezzo del percorso in base al killometraggio
     let prezzoBiglietto = (killometriDaPercorrere*prezzoPerKillometro)
-    console.log(prezzoBiglietto)
+    console.log(prezzoBiglietto
+
+    )
 
         //calcolo da fare nel caso eta inferiore a 18 anni
         let scontoInferiore = (prezzoBiglietto*80/100);
@@ -30,16 +32,25 @@
         //SE l'eta del passeggero e inferiore a 18 anni ha uno sconto del 20%
         if (etaDelPasseggero < 18) {
             //sconto del 20%
-            prezzo = scontoInferiore
+            prezzo = scontoInferiore.toFixed(2)
             //ALTRIMENTI SE l'eta del passeggero e superiore a 64 anni ha uno sconto del 40%
         }else if(etaDelPasseggero > 64) {
             //sconto del 40%
-            prezzo = scontoSuperiore
+            prezzo = scontoSuperiore.toFixed(2)
             //Altrimenti il prezzo rimane non scontato
         }else {
-            prezzo = prezzoBiglietto
+            prezzo = prezzoBiglietto.toFixed(2);
         }
 
 //OUTPUT
-    // prezzo finale 
-    console.log("Il prezzo da pagare e" + " " + prezzo + "€")
+        // prezzo finale se si ha meno di 18 anni
+    if(etaDelPasseggero < 18) {
+        console.log("Il prezzo da pagare scontato del 20% e" + " " + prezzo + "€")
+        // prezzo finale se si ha piu di 64 anni
+    }else if(etaDelPasseggero > 64) {
+        console.log("Il prezzo da pagare scontato del 40% e" + " " + prezzo + "€")
+        // prezzo finale se si ha l'eta tra 18 e 64 anni
+    }else{
+        console.log("Il prezzo da pagare e" + " " + prezzo + "€")
+    }
+    
