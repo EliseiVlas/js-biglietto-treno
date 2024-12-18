1c3942dbf22475a966d354e5e3c1341f377bb521
Elisei <eliseivlas.002@gmail.com>
2024-12-18 11:06:07 +0100

    corezione es

--- script.js
+++ script.js
@@ -1,56 +1,39 @@
 // Biglietto del treno
-
 // INPUT
     // prezzo biglietto in base ai km
-
-    let prezzoPerKillometro = 0.21;
+    const prezzoPerKillometro = 0.21;
 
     // l'utente inserisce il numero di killometri che vuole percorrere
-    let killometriDaPercorrere = prompt("Inserisca i killometri che deve percorrere");
-
+    const killometriDaPercorrere = parseInt(prompt("Inserisca i killometri che deve percorrere"));
+    
     //l'utente inserisce la sua eta
-    let etaDelPasseggero = prompt("Inserisca la sua eta") 
+    const etaDelPasseggero = parseInt(prompt("Inserisca la sua eta"));
 
-
-    console.log(prezzoPerKillometro, killometriDaPercorrere, etaDelPasseggero);
 //ELABORAZIONE
     // calcolo il prezzo del percorso in base al killometraggio
     let prezzoBiglietto = (killometriDaPercorrere*prezzoPerKillometro)
-    console.log(prezzoBiglietto
-
-    )
-
+    
         //calcolo da fare nel caso eta inferiore a 18 anni
         let scontoInferiore = (prezzoBiglietto*80/100);
         
-
         //calcolo da fare nel caso eta superiore a 64 anni
         let scontoSuperiore = (prezzoBiglietto*60/100)
         
-    
     // calcolo lo sconto in base all'eta del passegero
         //SE l'eta del passeggero e inferiore a 18 anni ha uno sconto del 20%
         if (etaDelPasseggero < 18) {
             //sconto del 20%
             prezzo = scontoInferiore.toFixed(2)
+            console.log("Il prezzo da pagare scontato del 20% e" + " " + prezzo + "€")
             //ALTRIMENTI SE l'eta del passeggero e superiore a 64 anni ha uno sconto del 40%
         }else if(etaDelPasseggero > 64) {
             //sconto del 40%
             prezzo = scontoSuperiore.toFixed(2)
+            console.log("Il prezzo da pagare scontato del 40% e" + " " + prezzo + "€")
+
             //Altrimenti il prezzo rimane non scontato
         }else {
             prezzo = prezzoBiglietto.toFixed(2);
+            console.log("Il prezzo da pagare e" + " " + prezzo + "€")
         }
-
-//OUTPUT
-        // prezzo finale se si ha meno di 18 anni
-    if(etaDelPasseggero < 18) {
-        console.log("Il prezzo da pagare scontato del 20% e" + " " + prezzo + "€")
-        // prezzo finale se si ha piu di 64 anni
-    }else if(etaDelPasseggero > 64) {
-        console.log("Il prezzo da pagare scontato del 40% e" + " " + prezzo + "€")
-        // prezzo finale se si ha l'eta tra 18 e 64 anni
-    }else{
-        console.log("Il prezzo da pagare e" + " " + prezzo + "€")
-    }
     
